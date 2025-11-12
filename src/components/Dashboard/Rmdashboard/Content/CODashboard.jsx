c3979fcffb33cef576093e56a6ce842e8886c64c
raphael <raphaeleric69578@gmail.com>
2025-11-12 10:50:25 +0300

    modified cddashboard

--- src/components/Dashboard/Rmdashboard/Content/CODashboard.jsx
+++ src/components/Dashboard/Rmdashboard/Content/CODashboard.jsx
@@ -12,7 +12,7 @@ const recentActivityRM = [
 
 const RMDashboard = () => (
   <div>
-    <h2 className="text-2xl font-bold mb-4">RM Dashboard</h2>
+    <h2 className="text-2xl font-semibold mb-4">RM Dashboard</h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
       {statsRM.map((stat, i) => (
         <div key={i} className="bg-green-600 text-white p-6 rounded-lg shadow">
